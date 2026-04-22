f9d64f02190befbad2482dc318835b98a0ef4111
jihadsoyon <jihadsoyon@gmail.com>
2026-04-22 09:56:26 +0600

    implement page structure with routing and navigation in Next.js

---
+++ Nextjs-learn-practice/nextjs-basics/src/app/about/developers/page.jsx
@@ -0,0 +1,10 @@
+
+const DevelopersPage = () => {
+    return (
+        <div>
+            <h2 className="text-5xl">This is Developers Page</h2>
+        </div>
+    );
+};
+
+export default DevelopersPage;
