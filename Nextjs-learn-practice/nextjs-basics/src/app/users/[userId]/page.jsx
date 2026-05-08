cd30d7bb8d53484c128d24795889ced68c51cb86
jihadsoyon <jihadsoyon@gmail.com>
2026-05-08 12:53:10 +0600

    Recap dynamic routing and data loading in Next.js

---
+++ Nextjs-learn-practice/nextjs-basics/src/app/users/[userId]/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const UsersDetailsPage = async({params}) => {
+    const {userId} = await params;
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+    const user = await res.json();
+    console.log('show me userId', userId)
+    return (
+        <div>
+            <p>{user.email}</p>
+            <p>{user.phone}</p>
+        </div>
+    );
+};
+
+export default UsersDetailsPage;
---
+++ Nextjs-learn-practice/nextjs-basics/src/app/users/page.jsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+
+const Userspage = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await res.json();
+    return (
+        <div>
+            <h2>Users are coming</h2>
+            <div className="grid grid-cols-3 gap-4">
+                {
+                    users.map(user => <div key={user.id}
+                        className="card bg-primary text-primary-content">
+                        <div className="card-body">
+                            <h2 className="card-title">{user.name}!</h2>
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                            <div className="card-actions justify-end">
+                                <Link href={`/users/${user.id}`}>
+                                <button className="btn">Show Info</button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Userspage;
