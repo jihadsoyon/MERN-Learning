348d4ca10f7706278d245cf33ee2589cb9e6d4e5
jihadsoyon <jihadsoyon@gmail.com>
2026-05-09 10:56:50 +0600

    Apply Google Fonts in Next.js project

--- Nextjs-learn-practice/nextjs-basics/src/app/blogs/page.jsx
+++ Nextjs-learn-practice/nextjs-basics/src/app/blogs/page.jsx
@@ -1,4 +1,14 @@
 import Link from "next/link";
+import { Roboto, Poppins } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
+const poppins = Poppins({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+})
 
 export const metadata = {
   title: 'Blogs',
@@ -55,7 +65,7 @@ const BlogsPage = () => {
 ];
 
     return (
-        <div>
+        <div className={poppins.className}>
             <h2 className="text-3xl font-bold mb-4">Blogs</h2>
             {blogs.map(blog => <div key={blog.id}>
                 <h3 className="text-4xl font-bold mb-2">{blog.title}</h3>
